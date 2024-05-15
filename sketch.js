1810f5e2daaea89001fb480c9322bc4348c4a23f
yolao08 <00001101324028sp@al.educacao.sp.gov.br>
2024-05-15 16:03:32 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+
+    createCanvas(400, 400);
+  
+    background("white")
+  
+  }
+  
+  function draw() {
+  
+    stroke("purple");
+  
+    fill("violet");
+  
+    
+  
+    
+  
+    if (mouseIsPressed) {
+  
+      rect(mouseX, mouseY, 20, 35);
+  
+    }
+  
+  }
+    
